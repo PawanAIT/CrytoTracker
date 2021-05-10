a4884831fb912ba06d51df586b9afe3bc6cca50a
Pawan Kumar <pawan161096@gmail.com>
2021-05-10 13:47:58 +0530

    update

--- Monitor.py
+++ Monitor.py
@@ -9,9 +9,9 @@ with open('buyOrders.json', 'r') as openfile:
     buyOrders = json.load(openfile)
   
 print(buyOrders)
-
 fig = plt.figure()
 ax = fig.add_subplot()
+#fig, ax = plt.subplots(nrows = 1, ncols = 1)
 
 symbolList = buyOrders.keys()
 symbolurl = ""
@@ -31,17 +31,31 @@ def on_message(ws, message):
     
     perChange = (new_price - buy_price) * 100 / buy_price
     #data[symbol] = {'cur_price' : cur_price, 'Bought_price' : buy_price, 'PercentageChange' : perChange}
-    data[symbol] = {'PercentageChange' : perChange}
+    data[symbol] = {'PercentageChange' : perChange, 'cur_price' : new_price, 'Bought_price' : buy_price}
     names = list(data.keys())
-    values = list([float(f'%.4f' % x['PercentageChange']) for x in data.values()])
-    ax.bar(names, values, color ='blue')
-    for i,j in zip(names,values):
-        ax.annotate(str(j),xy=(i,j))
-        #plt.text(i, j, str(j))
+    percentage = list([float(f'%.4f' % x['PercentageChange']) for x in data.values()])
+    currentPrice = list([float(f'%.4f' % x['cur_price']) for x in data.values()])
+    boughtPrice = list([float(f'%.4f' % x['Bought_price']) for x in data.values()])
+    barWidth = 0.25
+    ax.bar(names, percentage, color ='blue')
+    
+    #ax2.bar(names, currentPrice, color = 'pink')
+    #fig.canvas.draw()
+    
+    for i,j in zip(names,percentage):
+        ax.text(i, j, str(j))
+    # for i,j in zip(names,currentPrice):
+    #     ax2.text(i, j, str(j))
+    #     #plt.text(i, j, str(j))
+    # plt.cla()
+    # fig.cla()
+    #plt.draw()
+
     fig.canvas.draw()
     fig.show()
-    plt.pause(0.05)
-    plt.gca().cla()
+    plt.cla()
+    #plt.show()
+    plt.pause(0.10)
 
 def on_close(ws):
     print("### closed ###")
